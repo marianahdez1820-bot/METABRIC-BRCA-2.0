9b7101f73d6787fd5311e5f17e3fa94a85cbff5c
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-05-18 08:48:06 -0600

    Fix ROC curve plotting and update dataset labels
    Updated ROC curve plotting logic to include GSE2034 data and fixed variable names.

--- 6. Images/6.1 ROC_curves.R
+++ 6. Images/6.1 ROC_curves.R
@@ -15,16 +15,19 @@ pot_roc.gse <-
 
 facet_labels.gse <-
   if(!exists("facet_labels.gse96058") & exists("facet_labels.gse2034")){
-    plot_roc.gse2034
-  }else if(!exists("facet_labels.gse2034") & exists("facet_labels.gse96058")){
     facet_labels.gse2034
+    GSE_label <- "GSE2034"
+  }else if(!exists("facet_labels.gse2034") & exists("facet_labels.gse96058")){
+    
+    facet_labels.gse96058
+    GSE_label <- "GSE96058"
   }else{
     print("None or both objects exist, select 1")
   }
 
 # Object with all the ROC data from thje different databases
 
-plot_roc_total <- bind_rows(plot_roc, plot_roc.gse96058, plot_roc.tcga)
+plot_roc_total <- bind_rows(plot_roc, pot_roc.gse, plot_roc.tcga)
 
 # Object with all the label data from all the databases
 
@@ -42,7 +45,7 @@ ggplot(plot_roc_total, aes(x = FP, y = TP, color = data_set)) +
   geom_line(linewidth = 1) +
   geom_abline(slope = 1, intercept = 0) + 
   facet_wrap( ~ Time) +
-  scale_color_manual(values = c("METABRIC" = "#c388ff", "GSE96058" = "#DD6868", "TCGA" = "#B8D369")) +
+  scale_color_manual(values = c("METABRIC" = "#c388ff", "GSE2034" = "#DD6868", "TCGA" = "#B8D369")) +
   geom_text(data = facet_labels_final, 
             aes(x = 0.75, y = y_pos, label = AUC_Text, color = data_set), 
             size = 6, fontface = "bold") +
@@ -62,3 +65,17 @@ ggplot(plot_roc_total, aes(x = FP, y = TP, color = data_set)) +
     legend.text = element_text(size = 12)                 
   )
 
+########################################################################################
+
+global_roc_all <- bind_rows(global_roc, global_roc.gse2034, global_roc.tcga)
+
+
+
+
+
+ggplot(global_roc_all, aes(x = 1 - specificity, y = sensitivity, color = label)) +
+  geom_path() +
+  geom_abline(lty = 3) +
+  coord_equal() +
+  theme_classic() +
+  labs(title = "Global ROC curves")
