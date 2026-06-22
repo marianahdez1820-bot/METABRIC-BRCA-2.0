510dae3226d053457b84064debcec6cf88c91c81
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-06-22 14:38:40 -0600

    Add median summary and C-index calculations for datasets

--- 9. Results/9.1 C_score_AUC_tables.R
+++ 9. Results/9.1 C_score_AUC_tables.R
@@ -32,3 +32,42 @@ auc_df <-
 flextable(auc_df) %>%
   autofit() 
 
+summary_median.gse96058 <- summary(coxph(Surv(EVENT_MON, EVENT_STAT) ~ risk_group_median, data = gse96058_results))
+
+summary_median.tcga <- summary(coxph(Surv(EVENT_MON, EVENT_STAT) ~ risk_group_median, data = tcga_results))
+
+summary_median.metabric <- summary(coxph(Surv(EVENT_MON, EVENT_STAT) ~ risk_group_median, data = test_data))
+
+
+
+cox_table <- data.frame(
+  "Data base" = c("METABRIC", "TCGA", "GSE96058"),
+  
+  "HR (CI 95%, p-value )" = c(paste(summary_cox$coefficients[2], " (", summary_cox$conf.int[3], " - ", summary_cox$conf.int[4], ", p-value ", summary_cox$coefficients[5]),
+                              paste(summary_cox_tcga$coefficients[2], " (", summary_cox_tcga$conf.int[3], " - ", summary_cox_tcga$conf.int[4], ", p-value ", summary_cox_tcga$coefficients[5]),
+                              paste(summary_gse96058$coefficients[2], " (", summary_gse96058$conf.int[3], " - ", summary_gse96058$conf.int[4], ", p-value ", summary_gse96058$coefficients[5])),
+  
+  "C-Index" = c(summary_cox$concordance[1],
+                summary_cox_tcga$concordance[1],
+                summary_gse96058$concordance[1]),
+  
+  
+  
+  "HR (CI 95%, p-value)" = c(paste(summary_median.metabric$coefficients[2], " (", summary_median.metabric$conf.int[3], " - ", summary_median.metabric$conf.int[4], ", p-value ", summary_median.metabric$coefficients[5]),
+                             paste(summary_median.tcga$coefficients[2], " (", summary_median.tcga$conf.int[3], " - ", summary_median.tcga$conf.int[4], ", p-value ", summary_median.tcga$coefficients[5]),
+                             paste(summary_median.gse96058$coefficients[2], " (", summary_median.gse96058$conf.int[3], " - ", summary_median.gse96058$conf.int[4], ", p-value ", summary_median.gse96058$coefficients[5])),
+  
+  "C-Index " = c(summary_median.metabric$concordance[1],
+                 summary_median.tcga$concordance[1],
+                 summary_median.gse96058$concordance[1])
+  
+  
+)
+
+flextable(cox_table) %>%
+  autofit()
+
+
+
+
+
